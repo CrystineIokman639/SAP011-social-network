19ce7a45fa1f382a0938e2282626cdba3aee0ebc
laiscosta25 <laiscosta.c2@gmail.com>
2023-09-14 20:33:13 -0300

    Firebase Configurado

---
+++ firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBh62MQTgzQD5tQuQ5JmCEY-MtFaYc3jWU",
+  authDomain: "booknook-deb87.firebaseapp.com",
+  projectId: "booknook-deb87",
+  storageBucket: "booknook-deb87.appspot.com",
+  messagingSenderId: "458461086587",
+  appId: "1:458461086587:web:cd1ee96414be7ace2ff3de",
+  measurementId: "G-TX0TXHJD65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
