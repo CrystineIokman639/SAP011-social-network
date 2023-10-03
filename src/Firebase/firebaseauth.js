34d3d2710fe8bd6f0874a71f57fc89b2fadb6966
Gabriella Vieira Bruno <gabi_bvieira@hotmail.com>
2023-10-03 12:15:20 -0300

    fiz autenticação usuario

--- src/Firebase/firebaseauth.js
+++ src/Firebase/firebaseauth.js
@@ -1,17 +1,40 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword,signInWithPopup, GoogleAuthProvider  } from 'firebase/auth';
 import {auth,app} from './firebase.config';
 
 function authLogin (email,password){
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = JSON.stringify(userCredential.user);
+      window.localStorage.setItem("auth",user)
+      window.location.href="/#feed"
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
 }
 
-export {authLogin}
+function authByGoogle (){
+  const provider = new GoogleAuthProvider()
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    // acrescentei os dados a baixo para ir para o feed
+    const user = JSON.stringify(result.user);
+    window.localStorage.setItem("auth",user)
+    window.location.href="/#feed"
+
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  });
+}
+// recupera os dados de autenticação do usuario
+function getUserData(){
+  let data=window.localStorage.getItem("auth");
+  return JSON.parse(data);
+}
+
+export {authLogin,authByGoogle}
