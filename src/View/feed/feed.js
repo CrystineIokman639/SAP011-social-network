554169f445f7bb665f14890ca4e0923d17598a73
Crystine Iokman <132957762+CrystineIokman639@users.noreply.github.com>
2023-10-04 13:04:33 -0300

    js menu function recolher/mostrar

--- src/View/feed/feed.js
+++ src/View/feed/feed.js
@@ -3,29 +3,46 @@ import './feed.css';
 export default () => {
     const userFeed = document.createElement('section');
     const template = ` 
+  <section class="geral">
    <nav class="containerFeed">
      <header class="topHeader">
+       <img class="img-menu" src="img/menu.png" alt="logo menu três riscos iguais um em cima do outro" height="45" width="45" onclick="clickMenu()">
        <img class="logo-header" src="img/logo.svg" alt="logo booknook, um livro aberto com as folhas abrindo" height="60" width="60">
+       <img class="nada" src="img/logon.svg" alt="icone vazio gambiarra braba">
      </header>
+    <ul id="itens" class="menu">
+     <li class="cat">
+       <img class="feed" src="img/feed.png" alt="icone para ir para o feed">
+       <a class="titulo">Feed</a>
+     </li>
+     <li class="cat">
+       <img class="logout" src="img/logout.png" alt="icone para sair da página">
+       <a class="titulo" href="/#login">Sair</a>
+     </li>
+    </ul>
       <section class="template">
         <form class='form-feed'>
           <textarea class="user-text-area" placeholder="O que está lendo?"></textarea>
         </form>
       </section>
-    <ul class="menu">
-      <li>
-      <a class="cat">Feed</a>
-      </li>
-      <li>
-      <a class="cat">Sair</a>
-      </li>
-      <li>
-      <a class="cat">Meu perfil</a>
-      </li>
-    </ul>
-   </nav>`
+   </nav>
+  </section>`
 
     userFeed.innerHTML = template;
+    
+    const imgMenu = userFeed.querySelector('.img-menu');
+    imgMenu.onclick = clickMenu;
+
+    function clickMenu() {
+      const itens = document.getElementById('itens');
+      if (itens.style.display === "block") {
+          itens.style.display = "none";
+      } else {
+          itens.style.display = "block";
+      }
+  }
+
+
 return userFeed;
 }
 
